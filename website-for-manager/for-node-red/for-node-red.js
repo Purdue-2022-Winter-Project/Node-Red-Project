abfcd887d6e53896c9062c8ee4e5b0ed719a91b0
woo-jk <201702042@o.cnu.ac.kr>
2022-02-17 20:10:15 -0500

    add feature : car detective

--- website-for-manager/for-node-red/for-node-red.js
+++ website-for-manager/for-node-red/for-node-red.js
@@ -1,3 +1,43 @@
+// For Node-Red Web Socket
+var ws;
+var wsUri = "ws:";
+var loc = window.location;
+console.log(loc);
+if (loc.protocol === "https:") {
+  wsUri = "wss:";
+}
+// This needs to point to the web socket in the Node-RED flow
+wsUri += "//" + loc.host + loc.pathname.replace("manager", "ws/manager");
+
+function wsConnect() {
+  console.log("connect", wsUri);
+  ws = new WebSocket(wsUri);
+  //var line = "";    // either uncomment this for a building list of messages
+  ws.onmessage = function (msg) {
+    var line = ""; // or uncomment this to overwrite the existing message
+    // parse the incoming message as a JSON object
+    var data = msg.data;
+    console.log(data);
+    // build the output from the topic and payload parts of the object
+    line += "<p>" + data + "</p>";
+    // replace the messages div with the new "line"
+    // document.getElementById('messages').innerHTML = line;
+    //ws.send(JSON.stringify({data:data}));
+  };
+  ws.onopen = function () {
+    // update the status div with the connection status
+    // document.getElementById('status').innerHTML = "connected";
+    //ws.send("Open for data");
+    console.log("connected");
+  };
+  ws.onclose = function () {
+    // update the status div with the connection status
+    // document.getElementById('status').innerHTML = "not connected";
+    // in case of lost connection tries to reconnect every 3 secs
+    setTimeout(wsConnect, 3000);
+  };
+}
+////////
 var areaCount = 8;
 var redColor = "#ff0000";
 var greenColor = "#00c700";
@@ -20,6 +60,7 @@ function changeColor(color) {
   //     snackbar.className = snackbar.className.replace("hide", "");
   //   }, 500);
   updateSectionCount();
+  sendToNodered();
 }
 
 function selectSpot() {
@@ -126,16 +167,24 @@ function reset() {
   updateSectionCount();
 }
 
-function parking() {
-  var receive = document.getElementById("temp-text").value;
-  var car = document.getElementsByClassName("car");
-  var area = receive.split("");
-  for (var i = 0; i < areaCount; i++) {
-    var car = document.getElementsByClassName("car")[i];
-    if (area[i] == 0 && car.className == "car in") {
-      car.className = "car out";
-    } else if (area[i] == 1) {
-      car.className = "car in";
+function sendToNodered() {
+  var areaArr = [];
+  for (var i = 1; i <= areaCount; i++) {
+    var area = "A0" + i;
+    var backgroundColor = document.getElementById(area).style.backgroundColor;
+    var hex = rgbToHex(backgroundColor);
+    if (hex == redColor) {
+      areaArr[i - 1] = 1;
+    } else if (hex == greenColor) {
+      areaArr[i - 1] = 2;
+    } else if (hex == blueColor) {
+      areaArr[i - 1] = 3;
+    } else {
+      areaArr[i - 1] = 0;
     }
+  }
+  var sendmsg = areaArr.join(" ");
+  if (ws) {
+    ws.send(sendmsg);
   }
 }
