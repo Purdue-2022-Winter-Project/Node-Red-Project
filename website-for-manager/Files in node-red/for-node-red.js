b8d81f9b2f72e12636f848ec11792c906bb803d9
woo-jk <201702042@o.cnu.ac.kr>
2022-02-24 02:26:08 -0500

    Update Manager web & Add User Web

--- website-for-manager/Files in node-red/for-node-red.js
+++ website-for-manager/Files in node-red/for-node-red.js
@@ -1,3 +1,46 @@
+// For Node-Red Web Socket
+var ws;
+var wsUri = "ws:";
+var loc = window.location;
+console.log(loc);
+if (loc.protocol === "https:") {
+  wsUri = "wss:";
+}
+// This needs to point to the web socket in the Node-RED flow
+wsUri += "//" + loc.host + loc.pathname.replace("manager", "ws/manager");
+
+function wsConnect() {
+  console.log("connect", wsUri);
+  ws = new WebSocket(wsUri);
+  //var line = "";    // either uncomment this for a building list of messages
+  ws.onmessage = function (msg) {
+    var line = ""; // or uncomment this to overwrite the existing message
+    // parse the incoming message as a JSON object
+    var data = msg.data;
+    parking(data);
+    console.log(data);
+    // build the output from the topic and payload parts of the object
+    line += "<p>" + data + "</p>";
+    // replace the messages div with the new "line"
+    // document.getElementById('messages').innerHTML = line;
+    //ws.send(JSON.stringify({data:data}));
+  };
+  ws.onopen = function () {
+    // update the status div with the connection status
+    // document.getElementById('status').innerHTML = "connected";
+    //ws.send("Open for data");
+    console.log("connected");
+    sendToNodered();
+    sendToUser();
+  };
+  ws.onclose = function () {
+    // update the status div with the connection status
+    // document.getElementById('status').innerHTML = "not connected";
+    // in case of lost connection tries to reconnect every 3 secs
+    setTimeout(wsConnect, 3000);
+  };
+}
+////////
 var areaCount = 8;
 var redColor = "#ff0000";
 var greenColor = "#00c700";
@@ -19,23 +62,29 @@ var existColorList = ["red", "green", "blue"];
 var existNameList = ["Student", "Professor", "Visitor"];
 
 updateSectionCount();
+sendToNodered();
+sendToUser();
 
 function changeColor(color) {
-  //Change the color of parking spot in the "View part"
   for (var i = 1; i <= areaCount; i++) {
     var id = "r" + i;
     var area = "A0" + i;
     if (document.getElementById(id).checked) {
-      //If the parking spot is selected, change the color into argument
       document.getElementById(area).style.backgroundColor = color;
       document.getElementById(id).checked = false;
     }
   }
-
+  //   var x = document.getElementById("snackbar");
+  //   snackbar.className = "hide";
+  //   setTimeout(function () {
+  //     snackbar.className = snackbar.className.replace("hide", "");
+  //   }, 500);
   updateSectionCount();
+  sendToNodered();
+  sendToUser();
 }
-/*
-function selectSpot() {//
+
+function selectSpot() {
   var check = 0;
   var x = document.getElementById("snackbar");
   for (var i = 1; i <= areaCount; i++) {
@@ -53,12 +102,10 @@ function selectSpot() {//
   //     document.getElementsByClassName("color")[0].style.display = "none";
   //   }
 }
-*/
+
 function updateSectionCount() {
-  //Update the number of parking spot's color on the Section("Control part")
   var countArr = [0, 0, 0, 0, 0, 0, 0];
   for (var i = 1; i <= areaCount; i++) {
-    //Add the each color's number of selected parking spot
     var area = "A0" + i;
     var backgroundColor = document.getElementById(area).style.backgroundColor;
     var hex = rgbToHex(backgroundColor);
@@ -81,7 +128,6 @@ function updateSectionCount() {
   var sectionNumber = document.getElementsByClassName("section-number");
 
   for (let i = 0; i < sectionNumber.length; i++) {
-    //Set the innerHTML of each list
     var sectionNumberClass = sectionNumber[i].classList[1];
 
     if (sectionNumberClass == "number-red") {
@@ -103,7 +149,6 @@ function updateSectionCount() {
 }
 
 function rgbToHex(rgbType) {
-  //Convert RGB value into Hex value
   var rgb = rgbType.replace(/[^%,.\d]/g, "").split(",");
 
   rgb.forEach(function (str, x, arr) {
@@ -116,7 +161,6 @@ function rgbToHex(rgbType) {
 }
 
 function nameToHex(name) {
-  //Convert String value into Hex value
   if (name === "red") {
     return redColor;
   } else if (name === "green") {
@@ -133,7 +177,6 @@ function nameToHex(name) {
 }
 
 function rgbToName(rgb) {
-  //Convert RGB value into String value
   if (rgb === "rgb(255, 0, 0)") {
     return "red";
   } else if (rgb === "rgb(0, 199, 0)") {
@@ -150,7 +193,6 @@ function rgbToName(rgb) {
 }
 
 function hexToName(hex) {
-  //Convert Hex value into String value
   if (hex == redColor) {
     return "red";
   } else if (hex == greenColor) {
@@ -167,23 +209,19 @@ function hexToName(hex) {
 }
 
 function openPalette() {
-  //Open the "Color Setting"
   var paletteStyle = document.getElementsByClassName("color-palette")[0].style;
   var snackbar = document.getElementById("snackbar");
   var modeIcon = document.getElementById("mode-icon");
 
   if (paletteStyle.maxHeight) {
-    //If the mode is edit mode, change into view mode
     paletteStyle.maxHeight = null;
     for (var i = 1; i <= areaCount; i++) {
-      //Set the each parking spot to unclickable state
       var inputId = "r" + i;
       var labelId = "A0" + i;
       document.getElementById(inputId).disabled = true;
       document.getElementById(inputId).checked = false;
       document.getElementById(labelId).className = "viewmode col";
     }
-    //Inform the manager that the mode is changed into view mode
     document.getElementsByClassName("snackbar-item")[0].innerHTML =
       "<i class='fa fa-eye'></i> View Mode";
     snackbar.className = "show";
@@ -192,17 +230,14 @@ function openPalette() {
     }, 3000);
     modeIcon.className = "fa fa-eye";
   } else {
-    //If the mode is view mode, change into edit mode
     paletteStyle.maxHeight =
       document.getElementsByClassName("color-palette")[0].scrollHeight + "px";
     for (var i = 1; i <= areaCount; i++) {
-      //Set the each parking spot to clickable state
       var inputId = "r" + i;
       var labelId = "A0" + i;
       document.getElementById(inputId).disabled = false;
       document.getElementById(labelId).className = "whatever col";
     }
-    //Inform the manager that the mode is changed into edit mode
     document.getElementsByClassName("snackbar-item")[0].innerHTML =
       "<i class='fa fa-edit'></i> Color Setting Mode";
     snackbar.className = "show";
@@ -211,29 +246,27 @@ function openPalette() {
     }, 3000);
     modeIcon.className = "fa fa-edit";
   }
+  sendToUser();
 }
 
 function reset() {
-  //reset data of parking spot
   for (var i = 1; i <= areaCount; i++) {
-    //Reset to default color
     var id = "r" + i;
     var area = "A0" + i;
     document.getElementById(area).style.backgroundColor = "#d3d3d3";
     document.getElementById(id).checked = false;
   }
+  sendToNodered();
   updateSectionCount();
+  sendToUser();
 }
 
-function parking() {
-  //Check the presence of vehicle
-  var receive = document.getElementById("temp-text").value;
+function parking(receive) {
   var car = document.getElementsByClassName("car");
-  var area = receive.split(""); //Split the sent serial message from IR Infrared module
-  for (var i = 1; i < areaCount; i++) {
-    //Check whether the parking spot is parked
+  var area = receive.split("");
+  for (var i = 0; i < areaCount; i++) {
     var car = document.getElementsByClassName("car")[i];
-    if (area[i] == 0 && car.className == "car in") {
+    if (area[i] == 1 && car.className == "car in") {
       car.className = "car out";
     } else if (area[i] == 0) {
       car.className = "car in";
@@ -242,14 +275,12 @@ function parking() {
 }
 
 function addSection() {
-  //Add new list of the parking spot on the "Section"
   var sectionPopMenu = document.getElementsByClassName(
     "sectionList-add-popmenu"
   )[0];
   var sectionAdd = document.getElementById("section-add");
   // alert(sectionPopMenu.className == "sectionList-add-popmenu show");
   if (sectionPopMenu.className == "sectionList-add-popmenu show") {
-    //Close popup window
     sectionAdd.innerHTML =
       '<i class="fa fa-plus-square-o" aria-hidden="true"></i>';
     sectionPopMenu.className = "sectionList-add-popmenu hide";
@@ -260,7 +291,6 @@ function addSection() {
       );
     }, 500);
   } else {
-    //Open popup window
     sectionPopMenu.className = "sectionList-add-popmenu show";
     sectionAdd.innerHTML =
       '<i class="fa fa-angle-right" aria-hidden="true"></i>';
@@ -268,7 +298,6 @@ function addSection() {
 }
 
 function modifySection() {
-  //Modify the list of parking spot in the "Section"
   var sectionList = document.getElementsByClassName("section-list")[0];
   var sectionItem = document.getElementsByClassName("section-item");
   var sectionCircle = document.getElementsByClassName("section-circle");
@@ -277,15 +306,12 @@ function modifySection() {
   var sectionModify = document.getElementById("section-modify");
 
   if (edit.maxHeight) {
-    //If the "Section" is modify mode
     edit.maxHeight = null;
     sectionModify.innerHTML = '<i class="fa fa-edit" aria-hidden="true"></i>';
   } else {
-    //If the "Section" is view mode
     edit.maxHeight =
       document.getElementsByClassName("edit")[0].scrollHeight + "px";
     for (let i = 0; i < sectionItem.length - 2; i++) {
-      //Set the list to editable state
       sectionCircle[i].style.cursor = "pointer";
       sectionCircle[i].setAttribute("onClick", "changeSection(event)");
       sectionName[i].innerHTML =
@@ -313,27 +339,24 @@ function modifySection() {
 }
 
 function changeSection(event) {
-  //Change the color of parking spot list in the "Section"
   var targetColor = getComputedStyle(event.target).backgroundColor;
 
   for (let i = 0; i < allColorList.length; i++) {
-    //Change the color among remained color
     if (allColorList[i] == rgbToHex(targetColor)) {
       for (let j = 0; j < existColorList.length; j++) {
-        //If the color is already exist, move on the next color
         if (nameToHex(existColorList[j]) == allColorList[(i + 1) % 6]) {
           i++;
         }
       }
-      //Change the color
       event.target.style.backgroundColor = allColorList[(i + 1) % 6];
       return;
     }
   }
+  sendToNodered();
+  sendToUser();
 }
 
 function removeSection(event) {
-  //Remove the parking spot list of the "Section"
   var sectionList = document.getElementsByClassName("section-list")[0];
   var color = document.getElementsByClassName("color")[0];
   var targetNode = event.target;
@@ -343,18 +366,15 @@ function removeSection(event) {
   ).backgroundColor;
 
   for (let i = 0; i < existColorList.length; i++) {
-    //When the color which will be remove is same
     if (rgbToName(targetColor) === existColorList[i]) {
-      //Remove all the data of parking spot
       sectionList.removeChild(parentNode);
       color.removeChild(color.children[i]);
       existColorList.splice(i, 1);
       existNameList.splice(i, 1);
     }
   }
-  //If the color of parking spot which will be remove is set in the "View part"
+
   for (var i = 1; i <= areaCount; i++) {
-    //Change to default value
     var id = "r" + i;
     var area = "A0" + i;
     var removedColor = document.getElementById(area).style.backgroundColor;
@@ -363,11 +383,12 @@ function removeSection(event) {
       document.getElementById(id).checked = false;
     }
   }
+  sendToNodered();
   updateSectionCount();
+  sendToUser();
 }
 
 function confirmSectionListModify() {
-  //Confirm modifying of parking spot list
   var color = document.getElementsByClassName("color")[0].children;
   var sectionList = document.getElementsByClassName("section-list")[0];
   var sectionItem = document.getElementsByClassName("section-item");
@@ -381,21 +402,19 @@ function confirmSectionListModify() {
   existColorList = [];
   existNameList = [];
   for (let i = 0; i < sectionName.length - 2; i++) {
-    //Undo the list to original state in the "Section"
     sectionCircle[i].style.cursor = "default";
     sectionCircle[i].removeAttribute("onClick");
     var sectionColor = getComputedStyle(sectionCircle[i]).backgroundColor;
     sectionColor = rgbToName(sectionColor);
+
     sectionCircle[i].classList.remove(sectionCircle[i].classList[1]);
     sectionNumber[i].classList.remove(sectionNumber[i].classList[1]);
-
-    //Change the color data of parking spot in the "Section"
     sectionCircle[i].classList.add("circle-" + sectionColor);
     sectionNumber[i].classList.add("number-" + sectionColor);
+
     existColorList.push(sectionColor);
     existNameList.push(sectionName[i].children[0].value);
 
-    //Change the color data in the "Color Setting"
     color[i].classList.remove(color[i].classList[0]);
     color[i].classList.add(sectionColor);
     color[i].setAttribute(
@@ -406,12 +425,9 @@ function confirmSectionListModify() {
       sectionCircle[i]
     ).backgroundColor;
 
-    //Change the name data of parking spot in the "Section"
     sectionName[i].innerHTML = sectionName[i].children[0].value;
-    //remove the confirm button
     sectionItem[i].removeChild(sectionItem[i].childNodes[2]);
 
-    //Change the data of parking spot in the "View part"
     for (var j = 1; j <= areaCount; j++) {
       var id = "r" + j;
       var area = "A0" + j;
@@ -427,10 +443,11 @@ function confirmSectionListModify() {
   sectionModify.innerHTML = '<i class="fa fa-edit" aria-hidden="true"></i>';
   edit.maxHeight = null;
   // sectionList.removeChild(sectionList.lastChild);
+  sendToNodered();
+  sendToUser();
 }
 
 function confirmSectionListAdd() {
-  //Confirm adding of parking spot list
   var colorList = document.getElementsByClassName(
     "sectionList-add-popmenu-color"
   )[0].children;
@@ -444,8 +461,6 @@ function confirmSectionListAdd() {
     document.getElementsByClassName("selectedColor")[0].style.backgroundColor;
   var colorCheck = document.getElementsByClassName("color-check");
   var sectionAdd = document.getElementById("section-add");
-
-  //Check the duplicated value
   for (let i = 0; i < existNameList.length; i++) {
     if (existNameList[i] == newSectionInput) {
       return alert("Name is duplicated!");
@@ -455,23 +470,16 @@ function confirmSectionListAdd() {
     }
   }
 
-  //First clone the existing parking spot in the "Section"
   var newSectionItem =
     sectionList.children[sectionList.children.length - 2].cloneNode(true);
-
-  //Remove it's original data
   newSectionItem.children[0].children[0].classList.remove("circle-default");
   newSectionItem.children[1].classList.remove("number-default");
-
-  //Set the data of parking spot
   newSectionItem.children[0].children[0].classList.add(
     "circle-" + rgbToName(newSectionColor)
   );
   newSectionItem.children[1].classList.add(
     "number-" + rgbToName(newSectionColor)
   );
-
-  //Add the data of parking spot on the "Section"
   newSectionItem.children[1].innerHTML = 0;
   newSectionItem.children[0].children[1].childNodes[0].data = newSectionInput;
   newSectionItem.children[0].children[0].style.backgroundColor =
@@ -484,23 +492,15 @@ function confirmSectionListAdd() {
     sectionList.children[sectionList.children.length - 2]
   );
 
-  //First clone the existing parking spot in the "Color Setting"
   var color = document.getElementsByClassName("color")[0];
   var li = color.children[0].cloneNode(true);
-
-  //Remove it's original data
-  li.classList.remove(li.classList[0]);
-
-  //Set the data of parking spot
   li.style.backgroundColor = newSectionColor;
+  li.classList.remove(li.classList[0]);
   li.classList.add(rgbToName(newSectionColor));
   li.attributes[1].nodeValue =
     "changeColor('" + rgbToHex(newSectionColor) + "')";
-
-  //Add the data of parking spot on the "Color Setting"
   color.insertBefore(li, color.children[color.children.length - 1]);
 
-  //Undo all the values to original value in popup window
   for (let i = 0; i < colorList.length; i++) {
     colorList[i].classList.remove("selectedColor");
     colorCheck[i].style.display = "none";
@@ -515,16 +515,17 @@ function confirmSectionListAdd() {
   document.getElementById("newSectionName").value = "";
   sectionAdd.innerHTML =
     '<i class="fa fa-plus-square-o" aria-hidden="true"></i>';
+
+  sendToUser();
 }
 
 function newColorSelect(event) {
-  //When select the color in the popup window
   var colorList = document.getElementsByClassName(
     "sectionList-add-popmenu-color"
   )[0].children;
   var colorCheck = document.getElementsByClassName("color-check");
   var target = event.target;
-  //If the color is selected, style of selected color is changed
+
   for (let i = 0; i < colorCheck.length; i++) {
     if (colorCheck[i].style.display == "block") {
       colorList[i].classList.remove("selectedColor");
@@ -537,7 +538,6 @@ function newColorSelect(event) {
 }
 
 function closeAddPopMenu() {
-  //When the X button is clicked, close the add popup window
   var sectionAdd = document.getElementById("section-add");
   var colorList = document.getElementsByClassName(
     "sectionList-add-popmenu-color"
@@ -553,7 +553,6 @@ function closeAddPopMenu() {
       "sectionList-add-popmenu"
     );
   }, 500);
-  //Reset the selected color in the popup window
   for (let i = 0; i < colorList.length; i++) {
     colorList[i].classList.remove("selectedColor");
     colorCheck[i].style.display = "none";
@@ -561,4 +560,68 @@ function closeAddPopMenu() {
   document.getElementById("newSectionName").value = "";
   sectionAdd.innerHTML =
     '<i class="fa fa-plus-square-o" aria-hidden="true"></i>';
+}
+
+function sendToNodered() {
+  var areaArr = [];
+  for (var i = 1; i <= areaCount; i++) {
+    var area = "A0" + i;
+    var backgroundColor = document.getElementById(area).style.backgroundColor;
+    var hex = rgbToHex(backgroundColor);
+    if (hex == redColor) {
+      areaArr[i - 1] = 1;
+    } else if (hex == greenColor) {
+      areaArr[i - 1] = 2;
+    } else if (hex == blueColor) {
+      areaArr[i - 1] = 3;
+    } else if (hex == yellowColor) {
+      areaArr[i - 1] = 4;
+    } else if (hex == purpleColor) {
+      areaArr[i - 1] = 5;
+    } else if (hex == cyanColor) {
+      areaArr[i - 1] = 6;
+    } else {
+      areaArr[i - 1] = 0;
+    }
+  }
+  var sendmsg = areaArr.join(" ");
+  if (ws) {
+    ws.send(sendmsg);
+  }
+}
+
+function sendToUser() {
+  var areaArr = [];
+  for (var i = 1; i <= areaCount; i++) {
+    var area = "A0" + i;
+    var backgroundColor = document.getElementById(area).style.backgroundColor;
+    var hex = rgbToHex(backgroundColor);
+    if (hex == redColor) {
+      areaArr[i - 1] = 1;
+    } else if (hex == greenColor) {
+      areaArr[i - 1] = 2;
+    } else if (hex == blueColor) {
+      areaArr[i - 1] = 3;
+    } else if (hex == yellowColor) {
+      areaArr[i - 1] = 4;
+    } else if (hex == purpleColor) {
+      areaArr[i - 1] = 5;
+    } else if (hex == cyanColor) {
+      areaArr[i - 1] = 6;
+    } else {
+      areaArr[i - 1] = 0;
+    }
+  }
+  var area = areaArr.join("");
+  var text =
+    '{"area":"' +
+    area +
+    '", "color":"' +
+    existColorList +
+    '", "name":"' +
+    existNameList +
+    '"}';
+  if (ws) {
+    ws.send(text);
+  }
 }
